013b32478b01d96fff35352eb5c337d99dbd40b4
Arsh <kaurarshpreet746@gmail.com>
2025-06-19 10:39:49 +0530

    changes

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function page({ params }: any) {
   return (
-    <div>
-      <h1>{params.id}</h1>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Card>
+          <CardHeader className="text-center">
+            <CardTitle className="text-xl">{params.id}</CardTitle>
+          </CardHeader>
+        </Card>
+      </div>
     </div>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -30,18 +31,37 @@ export default function page() {
   };
 
   return (
-    <div>
-      <h1>Profile</h1>
-      <h2>
-        {data === "nothing" ? (
-          "Nothing"
-        ) : (
-          <Link href={`/profile/${data}`}>{data}</Link>
-        )}
-      </h2>
-      <hr />
-      <Button onClick={logout}>Logout</Button>
-      <Button onClick={getUserDetails}>User details</Button>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Card>
+          <CardHeader className="text-center">
+            <CardTitle className="text-xl">Profile</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <h2 className="text-lg text-muted-foreground text-center mb-3">
+              {data === "nothing" ? (
+                "Nothing"
+              ) : (
+                <Link
+                  href={`/profile/${data}`}
+                  className="text-primary hover:underline"
+                >
+                  {data}
+                </Link>
+              )}
+            </h2>
+
+            <hr />
+
+            <div className="flex gap-6 mt-5 justify-center">
+              <Button onClick={logout}>Logout</Button>
+              <Button onClick={getUserDetails} variant="outline">
+                User details
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
